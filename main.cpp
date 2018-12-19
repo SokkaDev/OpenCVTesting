465a7f8041332d8e1b9c2ce0f92a6c661a9269fb
SokkaDev <barisgalaxys3@gmail.com>
2018-12-19 19:32:02 +0100

    Camera Access + Window Location of cam [Window Size must be fixed]

--- main.cpp
+++ main.cpp
@@ -1,8 +1,31 @@
+#include <opencv2/highgui.hpp>
 #include <iostream>
 
-
-int main() {
-
+using namespace cv;
 
 
+int main( int argc, char** argv ) {
+  
+  VideoCapture capture;
+
+    ///0 öffnet die default cam
+
+    string windowname="Test";
+
+  cvNamedWindow(windowname);
+  moveWindow(windowname,1500,2000);
+    if(!capture.open(0))
+        return 0;
+    for(;;)
+    {
+          Mat frame;
+          capture >> frame;
+          
+          
+          if( frame.empty() ) break; // Ende vom stream
+          imshow(windowname, frame);
+          
+          if( waitKey(10) == 27 ) break; // Stopp über Escape
+    }
+  return 0;
 }
